f0abb25f0ba9b4f92dce8a9709f65c0e3042f634
youssefdoass <youssefdoas@gmail.com>
2025-10-16 09:23:12 +0200

    Created Login

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -31,7 +31,21 @@ const loginSchema = Joi.object({
 
 // TODO: implement login function
 export async function login(req, res, next) {
- 
+  try {
+    const { value, error } = loginSchema.validate(req.body);
+    if (error) return res.status(400).json({ message: error.message });
+
+    const user = await User.findOne({ email: value.email });
+    if (!user) return res.status(401).json({ message: 'Invalid email or password' });
+
+    const isMatch = await user.comparePassword(value.password);
+    if (!isMatch) return res.status(401).json({ message: 'Invalid email or password' });
+
+    const token = signToken(user);
+    return res.json({ token, user: publicUser(user) });
+  } catch (err) {
+    next(err);
+  }
 }
 
 export async function me(req, res) {
